d0464fcd74c62dc8dc2f332b79a9cc7a7e42d7e3
Alvin Lim <alvink77@gmail.com>
2016-12-20 07:31:13 +0700

    fixed favico

--- routes/index.js
+++ routes/index.js
@@ -11,57 +11,60 @@ const API_KEY = process.env.CSE_API_KEY;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
 
 
-  router.get('/latest', (req, res, next) => {
-    searches.find({}, '-_id', (e, docs) => {
-      res.json(docs);
-    })
+router.get('/latest', (req, res, next) => {
+  searches.find({}, '-_id', (e, docs) => {
+    res.json(docs);
+  })
+});
 
+router.get('/favicon.ico', function(req, res) {
+    res.render('index');
+});
+
+router.get('/:query', (req, res, next) => {
+
+  var q = req.params.query;
+  var offset = (isNaN(req.query.offset))? 10 : req.query.offset;
+  const searchType = 'image';
+  var itemList = {};
+
+  searches.insert({
+    term: q,
+    when: new Date()
   });
 
-  router.get('/:query', (req, res, next) => {
+  const url = `https://www.googleapis.com/customsearch/v1?cx=${CSE_ID}&num=${offset}&searchType=image&key=${API_KEY}&q=${q}`;
 
-    var q = req.params.query;
-    var offset = (isNaN(req.query.offset))? 10 : req.query.offset;
-    const searchType = 'image';
-    var itemList = {};
+  var requestObject = {
+    url: url,
+    method: 'GET',
+    timeout: 10000
+  };
 
-    searches.insert({
-      term: q,
-      when: new Date()
-    });
+  request(requestObject, (err, response, body) => {
+    if (err) throw (err);
+    else {
+      var result = [];
+      itemList = JSON.parse(body).items;
 
-    const url = `https://www.googleapis.com/customsearch/v1?cx=${CSE_ID}&num=${offset}&searchType=image&key=${API_KEY}&q=${q}`;
-
-    var requestObject = {
-      url: url,
-      method: 'GET',
-      timeout: 10000
-    };
-
-    request(requestObject, (err, response, body) => {
-      if (err) throw (err);
+      if (itemList == null) { res.send(JSON.parse(body)); }
       else {
-        var result = [];
-        itemList = JSON.parse(body).items;
-
-        if (itemList == null) { res.send(JSON.parse(body)); }
-        else {
-          itemList.forEach((item) => {
-            result.push({
-              url: item.link,
-              snippet: item.snippet,
-              thumbnail: item.image.thumbnailLink,
-              context: item.image.contextLink
-            });
+        itemList.forEach((item) => {
+          result.push({
+            url: item.link,
+            snippet: item.snippet,
+            thumbnail: item.image.thumbnailLink,
+            context: item.image.contextLink
           });
-          res.send(result);
-        }
+        });
+        res.send(result);
       }
-    });
+    }
+  });
 });
 
 module.exports = router;
